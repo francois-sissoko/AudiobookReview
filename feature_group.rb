8d2445def9781b0c1e8e1e5b7707833ae8e4a717
PrivateContactMeFirstToUse <fsissoko007@yahoo.com>
2018-05-07 18:51:12 -0600

    adding the final files of the model (#16)
    Adding the final files of the models part of the app.

---
+++ feature_group.rb
@@ -0,0 +1,29 @@
+class FeatureGroup < ActiveRecord::Base
+	has_many :features, dependent: :destroy, autosave: true 
+	belong_to :product, foreign_key: "product_id"
+	accepts_nested_attributes_for :features, allow_destroy: true
+	
+	attr_accessible :name, :features, :description, :product_id, :product, :up, :singles, features: [:pictures]
+	
+	def can_user_vote(current_user)
+	if !current_user.nil?
+		return true
+		self.features.each do |f|
+			return false if f.upvotes.includes(user_id: current_user.id) or f.downvotes.include(user_id: current_user.id)
+		end
+		return true
+	end
+	return false
+	end
+	
+	def can_user_vote_andy(current_user)
+		if !current_user.nil?
+			self.features.each do |f|
+				return false if f.upvotes.includes(user_id: current_user.id) or f.downvotes.includes(user_id: current_user.id)
+			end 
+			return true
+		end
+		return false
+	end
+end
+
---
+++ image_asset.rb
@@ -0,0 +1,35 @@
+class ImageAsset < ActiveRecord::Base
+		belongs_to :attachable
+		has_attached_file :attachment,
+		styles: lambda { |a| {large: "x600>", medium: "x300>", thumb: "x100>", blurry: "x300#" }},
+	convert_options: { blurry: "-blur 0x6"}
+		default_url: "/images/missing-product.jpg"
+	storage: :s3
+	s3_endpoint: 's3-us-west-2.amazonaws.com',
+		s3_credentials: {s3_endpoint:		's3-us-west-2.amazonaws.com',
+																	bucket:		ENV['AWS_BUCKET']
+													access_key_id:	ENV['AWS_ACCESS_KEY_ID'],
+													secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']},
+	    s3_protocol: "https"
+attr_accessible :attachment, :user_id, :attachable_id, :id, :product_id, :attachment_attributes
+		
+include Rails.application.routes.url_helpers
+
+  def to_jq_upload
+	{
+		"name" => read_attribute(:attachment_file_name),
+		"size" => read_attribute(:attachment_file_size),
+		"url" => self.attachment.url(:thumb),
+		"delete_url" => image_asset_path(self),
+		"delete_type" => "DELETE"
+	}
+  end
+	
+  def image_asset_from_url(url)
+		self.attachment = open(url)
+  end
+
+  def self.find_product_user_pic(product)
+	find(product.user.propic_id || product.user.pictures.last)
+  end
+end							
