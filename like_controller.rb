cbaf1017c86071f631c11314c51e83db2718b1eb
PrivateContactMeFirstToUse <fsissoko007@yahoo.com>
2018-05-08 17:12:15 -0600

    Adding start of Controllers on Leaf 6 (#17)
    Controllers on Leaf 6 and bleach wallpaper

---
+++ like_controller.rb
@@ -0,0 +1,22 @@
+class LikesContoller < ApplicationController
+	def create 
+		if signed_in?
+			@like = Like.new(like_params)
+			@like.user_id = current_user.id
+			respond_to do |format|
+				if @like.save!
+					format.html { redirect_to @product, notice: 'Product was liked'}
+					format.json { head :no_content }
+				else
+					format.html { redirect_to @product, notice: 'Product couldn\'t be liked' }
+					format.json { head :no_content } 
+				end
+			end
+		end
+	end
+	
+	private 
+		def like_params
+			params.require(:like).permit(:id, :likeable_id, :product_id, :likeable_type, :user_id, :up)
+		end
+end
